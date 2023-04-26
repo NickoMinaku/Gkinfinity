06bbbeae7a39f721751938896db45b4133cce86d
NickoMinaku <trueshchev@mail.ru>
2023-04-26 19:09:19 +0500

    Add main screen page

---
+++ Gkinfinity/gkinfinity/wwwroot/js/site.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    $("button.schedule-button").click(function (event) {
+        event.preventDefault();
+        $("#dark-overlay").fadeIn(297, function () {
+            $("#schedule-popup")
+                .css("display", "block")
+                .animate({ opacity: 1 }, 198);
+        });
+    });
+
+    $("#main-screen-popup-cross, #dark-overlay").click(function () {
+        $("#schedule-popup").animate({ opacity: 0 }, 198, function () {
+            $(this).css("display", "none");
+            $("#dark-overlay").fadeOut(297);
+        });
+    });
+});
