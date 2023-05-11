c733cce9fae772f71c6dbf9544f27c16a8872dfd
NickoMinaku <trueshchev@mail.ru>
2023-05-12 00:17:44 +0500

    start adaptive

--- gkinfinity/gkinfinity/Services/MailSendRequest.cs
+++ gkinfinity/gkinfinity/Services/MailSendRequest.cs
@@ -6,6 +6,8 @@ using MailKit;
 using Microsoft.AspNetCore.Hosting.Server;
 using System.Web;
 using MailKit.Security;
+using System.Security.Authentication;
+using gkinfinity.Controllers;
 
 namespace gkinfinity.Services
 {
@@ -23,10 +25,12 @@ namespace gkinfinity.Services
                 Text = $"<h1><b>Заявка по франшизе {subject}</b></h1></br></br><div><b>Имя:</b> {sendRequest.Name}</br><b>Телефон:</b> {sendRequest.PhoneNumber}</br><b>Город:</b> {sendRequest.City}</div>"
             };
 
-            using (var client = new SmtpClient(new ProtocolLogger("log.log")))
+            using (var client = new SmtpClient())
             {
+                client.SslProtocols |= SslProtocols.Ssl3 | SslProtocols.Ssl2 | SslProtocols.Tls | SslProtocols.Tls11 | SslProtocols.Tls12;
                 await client.ConnectAsync("smtp.mail.ru", 25, false);
-                await client.AuthenticateAsync("Gkinfinity@mail.ru", "a0upaTbg2egcEAdduFR5");
+                /*await client.AuthenticateAsync("Gkinfinity@mail.ru", "a0upaTbg2egcEAdduFR5");*/
+                await client.AuthenticateAsync(new System.Net.NetworkCredential("Gkinfinity@mail.ru", "a0upaTbg2egcEAdduFR5"));
                 await client.SendAsync(emailMessage);
 
                 await client.DisconnectAsync(true);
